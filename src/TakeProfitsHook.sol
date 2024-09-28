d843fb8ada952363b02e585f4d1d679202fe1852
AjiteshBD <ajiteshmishra31@gmail.com>
2024-09-28 23:01:12 +0530

    :construction: WIP

--- src/TakeProfitsHook.sol
+++ src/TakeProfitsHook.sol
@@ -6,7 +6,26 @@ import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 import {Hooks} from "v4-core/libraries/Hooks.sol";
 
+import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+
 contract TakeProfitsHook is BaseHook, ERC1155 {
+    // Use the PoolIdLibrary for PoolKey to add the `.toId()` function on a PoolKey
+    // which hashes the PoolKey struct into a bytes32 value
+    using PoolIdLibrary for PoolKey;
+
+    // Create a mapping to store the last known tickLower value for a given Pool
+    mapping(PoolId poolId => int24 tickLower) public tickLowerLasts;
+
+    // Create a nested mapping to store the take-profit orders placed by users
+    // The mapping is PoolId => tickLower => zeroForOne => amount
+    // PoolId => (...) specifies the ID of the pool the order is for
+    // tickLower => (...) specifies the tickLower value of the order i.e. sell when price is greater than or equal to this tick
+    // zeroForOne => (...) specifies whether the order is swapping Token 0 for Token 1 (true), or vice versa (false)
+    // amount specifies the amount of the token being sold
+    mapping(PoolId poolId => mapping(int24 tick => mapping(bool zeroForOne => int256 amount))) public
+        takeProfitPositions;
+
     // Initialize BaseHook and ERC1155 parent contracts in the constructor
     constructor(IPoolManager _poolManager, string memory _uri) BaseHook(_poolManager) ERC1155(_uri) {}
 
