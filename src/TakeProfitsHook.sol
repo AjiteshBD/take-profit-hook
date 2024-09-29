4058d33d38f782a3631aab70d16bcc941a4ffe99
AjiteshBD <ajiteshmishra31@gmail.com>
2024-09-29 23:09:02 +0530

    :construction: WIP

--- src/TakeProfitsHook.sol
+++ src/TakeProfitsHook.sol
@@ -8,13 +8,32 @@ import {Hooks} from "v4-core/libraries/Hooks.sol";
 
 import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
 import {PoolKey} from "v4-core/types/PoolKey.sol";
+import {Currency, CurrencyLibrary} from "v4-core/types/Currency.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract TakeProfitsHook is BaseHook, ERC1155 {
     // Use the PoolIdLibrary for PoolKey to add the `.toId()` function on a PoolKey
     // which hashes the PoolKey struct into a bytes32 value
     using PoolIdLibrary for PoolKey;
+    using CurrencyLibrary for Currency;
+
+    struct TokenData {
+        PoolKey poolKey;
+        int24 tick;
+        bool zeroForOne;
+    }
+
+    // tokenIdExists is a mapping to store whether a given tokenId (i.e. a take-profit order) exists given a token id
+    mapping(uint256 tokenId => bool exists) public tokenIdExists;
+    // tokenIdClaimable is a mapping that stores how many swapped tokens are claimable for a given tokenId
+    mapping(uint256 tokenId => uint256 claimable) public tokenIdClaimable;
+    // tokenIdTotalSupply is a mapping that stores how many tokens need to be sold to execute the take-profit order
+    mapping(uint256 tokenId => uint256 supply) public tokenIdTotalSupply;
+    // tokenIdData is a mapping that stores the PoolKey, tickLower, and zeroForOne values for a given tokenId
+    mapping(uint256 tokenId => TokenData) public tokenIdData;
 
     // Create a mapping to store the last known tickLower value for a given Pool
+
     mapping(PoolId poolId => int24 tickLower) public tickLowerLasts;
 
     // Create a nested mapping to store the take-profit orders placed by users
@@ -28,6 +47,11 @@ contract TakeProfitsHook is BaseHook, ERC1155 {
 
     // Initialize BaseHook and ERC1155 parent contracts in the constructor
     constructor(IPoolManager _poolManager, string memory _uri) BaseHook(_poolManager) ERC1155(_uri) {}
+
+    // ERC-1155 Helpers
+    function getTokenId(PoolKey calldata key, int24 tickLower, bool zeroForOne) public pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(key.toId(), tickLower, zeroForOne)));
+    }
 
     // Required override function for BaseHook to let the PoolManager know which hooks are implemented
     function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
